72f80223d03827e15b6cfa603a9c8580104d8ce4
poornimaramamurthy355-eng <poornimaramamurthy355@gmail.com>
2026-06-23 12:15:22 +0530

    Base grade calculator

---
+++ grade-project/grade_calculator.py
@@ -0,0 +1,10 @@
+def calculate_grade(marks):
+    if marks >= 90:
+        return "A"
+    elif marks >= 75:
+        return "B"
+    elif marks >= 60:
+        return "C"
+    else:
+        return "F"
+    print(calculate_grade(85)) 
